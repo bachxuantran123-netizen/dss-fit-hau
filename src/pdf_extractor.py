2d3c91b4c3f9f58b55a8e8da97055c74130fd847
bachxuantran123 -netizen <bachxuantran123@gmail.com>
2026-09-14 23:20:23 +0700

    refactor: remove redundant mapping key for OOP

--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -37,7 +37,6 @@ SUBJECT_MAPPING: dict[str, str] = {
     "kỹ thuật lập trình": "KỸ THUẬT LẬP TRÌNH",
     "kỹ thuật đồ hoạ": "KỸ THUẬT ĐỒ HOẠ MÁY TÍNH",
     "kỹ thuật xử lý ảnh": "XỬ LÝ ẢNH",
-    "lập trình hướng": "LẬP TRÌNH HƯỚNG ĐỐI TƯỢNG",
     "lập trình hướng đối tượng": "LẬP TRÌNH HƯỚNG ĐỐI TƯỢNG",
     "quản trị mạng máy tính": "QUẢN TRỊ MẠNG MÁY TÍNH",
     "quản trị mạng": "QUẢN TRỊ MẠNG MÁY TÍNH",
